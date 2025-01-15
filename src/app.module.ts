846ec3b04e0a83cf531a7f1e0fd95b79bb40b958
김승호 <kimsh010425@gmail.com>
2025-01-15 23:05:12 +0900

    미들웨어 루트 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayersModule } from './players/players.module';
 import { TournamentsModule } from './tournaments/tournaments.module';
-
+import { AdminMiddleware } from './middlewares/admin.middleware';
 
 @Module({
   imports: [PlayersModule, TournamentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
-
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminMiddleware).forRoutes('tournaments');
+  }
+}
