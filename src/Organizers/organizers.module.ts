c0ff705c420f3a0d315db88840be09490586c1ce
김승호 <kimsh010425@gmail.com>
2025-01-17 18:59:13 +0900

    typeorm 모듈 추가 및 데이터베이스 연동

--- src/Organizers/organizers.module.ts
+++ src/Organizers/organizers.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizersController } from './organizers.controller';
 import { OrganizersService } from './organizers.service';
+import { Game } from 'src/entities/game.entity';  // 새로 만든 Game 엔티티
+import { Tournament } from 'src/entities/tournament.entity'; // 만약 여기서 직접 접근할 거라면
+import { Player } from 'src/entities/player.entity'; 
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Game, Tournament, Player]),
+      ],
   controllers: [OrganizersController],
   providers: [OrganizersService],
 })
